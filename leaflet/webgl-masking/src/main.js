20419f3fbff199528ed6f5538fbb807be56c0268
Gaku Yasui <gaku.yasui@gmail.com>
2025-11-07 09:45:22 +0900

    chore: add another rectangle

--- leaflet/webgl-masking/src/main.js
+++ leaflet/webgl-masking/src/main.js
@@ -33,19 +33,43 @@ canvas.height = 512;
 const ctx = canvas.getContext('2d', { willReadFrequently: true });
 tempMap.drawLayer();
 ctx.drawImage(tempMap._canvas, 0, 0, canvas.width, canvas.height);
-const maskArea = [[20, 20], [70, 70]];
-ctx.globalCompositeOperation = 'destination-in';
-const maskBounds = [
-  [(maskArea[0][0] - bounds[0][0]) / (bounds[1][0] - bounds[0][0]),
-    (maskArea[0][1] - bounds[0][1]) / (bounds[1][1] - bounds[0][1])],
-  [(maskArea[1][0] - bounds[0][0]) / (bounds[1][0] - bounds[0][0]),
-    (maskArea[1][1] - bounds[0][1]) / (bounds[1][1] - bounds[0][1])]
+
+const maskArea1 = [[20, 20], [40, 40]];
+const maskArea2 = [[60, 60], [90, 90]];
+
+const maskCanvas = document.createElement('canvas');
+maskCanvas.width = canvas.width;
+maskCanvas.height = canvas.height;
+const maskCtx = maskCanvas.getContext('2d');
+
+maskCtx.fillStyle = 'white';
+
+const maskBounds1 = [
+  [(maskArea1[0][0] - bounds[0][0]) / (bounds[1][0] - bounds[0][0]),
+    (maskArea1[0][1] - bounds[0][1]) / (bounds[1][1] - bounds[0][1])],
+  [(maskArea1[1][0] - bounds[0][0]) / (bounds[1][0] - bounds[0][0]),
+    (maskArea1[1][1] - bounds[0][1]) / (bounds[1][1] - bounds[0][1])]
 ];
-const maskX = maskBounds[0][1] * canvas.width;
-const maskY = (1 - maskBounds[1][0]) * canvas.height;
-const maskWidth = (maskBounds[1][1] - maskBounds[0][1]) * canvas.width;
-const maskHeight = (maskBounds[1][0] - maskBounds[0][0]) * canvas.height;
-ctx.fillRect(maskX, maskY, maskWidth, maskHeight);
+const maskX1 = maskBounds1[0][1] * canvas.width;
+const maskY1 = (1 - maskBounds1[1][0]) * canvas.height;
+const maskWidth1 = (maskBounds1[1][1] - maskBounds1[0][1]) * canvas.width;
+const maskHeight1 = (maskBounds1[1][0] - maskBounds1[0][0]) * canvas.height;
+maskCtx.fillRect(maskX1, maskY1, maskWidth1, maskHeight1);
+
+const maskBounds2 = [
+  [(maskArea2[0][0] - bounds[0][0]) / (bounds[1][0] - bounds[0][0]),
+    (maskArea2[0][1] - bounds[0][1]) / (bounds[1][1] - bounds[0][1])],
+  [(maskArea2[1][0] - bounds[0][0]) / (bounds[1][0] - bounds[0][0]),
+    (maskArea2[1][1] - bounds[0][1]) / (bounds[1][1] - bounds[0][1])]
+];
+const maskX2 = maskBounds2[0][1] * canvas.width;
+const maskY2 = (1 - maskBounds2[1][0]) * canvas.height;
+const maskWidth2 = (maskBounds2[1][1] - maskBounds2[0][1]) * canvas.width;
+const maskHeight2 = (maskBounds2[1][0] - maskBounds2[0][0]) * canvas.height;
+maskCtx.fillRect(maskX2, maskY2, maskWidth2, maskHeight2);
+
+ctx.globalCompositeOperation = 'destination-in';
+ctx.drawImage(maskCanvas, 0, 0);
 const imageData = canvas.toDataURL();
 var map = L.map('map', {
 	crs: L.CRS.Simple,
@@ -60,3 +84,4 @@ L.imageOverlay(imageData, bounds, {
   opacity: 0.35,
   interactive: false
 }).addTo(map);
+
